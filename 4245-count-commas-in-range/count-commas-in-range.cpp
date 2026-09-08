fee7202d8b5a459ab43a652b1672b876d6c8af0d
Sanket  Sudhir More <moresanket297@gmail.com>
2026-09-08 18:23:58 +0530

    Time: 8 ms (8.28%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans = 0;
+
+        for(int i = 0; i <= n; i++) {
+            
+            if(i >= 1000) {
+                ans++;
+            }
+
+            if(i >= 1000000) {
+                ans++;
+            }
+            
+            if(i >= 1000000000) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
